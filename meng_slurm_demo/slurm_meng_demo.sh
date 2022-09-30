7481ecd1aff9939f06344b7007d6f0ac1542452b
Yefan <39088217+YefanZhou@users.noreply.github.com>
2022-09-30 11:10:50 -0700

    rm -d

--- meng_slurm_demo/slurm_meng_demo.sh
+++ meng_slurm_demo/slurm_meng_demo.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH -p rise # partition (queue)
-#SBATCH -D /home/eecs/yefan0726/meng_slurm_demo
 ##SBATCH --exclude=havoc,r4,r16,atlas
 #SBATCH --nodelist=bombe
 #SBATCH -n 1 # number of tasks (i.e. processes)
